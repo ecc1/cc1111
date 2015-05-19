383a3bf5e843278598283b4795069752cb56a75d
Eric Cooper <ecc@cmu.edu>
2015-05-19 12:45:30 -0400

    define macros for enabling and disabling interrupts

--- arch.h
+++ arch.h
@@ -3,12 +3,20 @@
 
 #include "cc1111.h"
 
-#define nop()	__asm nop __endasm
+#define disable_interrupts()	EA = 0
+
+#define enable_interrupts()	EA = 1
+
+#define idle()			PCON = PCON_IDLE
+
+#define nop()			__asm nop __endasm
+
+// call idle() from a __critical section
 
 #define await_interrupt() do { \
-	EA = 1; \
-	PCON = PCON_IDLE; \
-	EA = 0; \
+	enable_interrupts(); \
+	idle();	\
+	disable_interrupts(); \
 } while (0)
 
 #endif /* _ARCH_H */
